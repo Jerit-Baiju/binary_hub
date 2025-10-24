f8aa3375b9d96c86a766656d82f1c557f386c5c6
Jerit Baiju <jeritalumkal@gmail.com>
2025-10-24 08:43:06 +0530

    Update .gitignore to include .DS_Store and clean up settings.py

--- main/settings.py
+++ main/settings.py
@@ -132,9 +132,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
